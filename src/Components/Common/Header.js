48ffa07f33032f968ffdc246cfe317af245a079c
James Spalding <jamescodebase@outlook.com>
2018-04-23 16:11:31 +0100

    Styled 404 page and added redirect link to main logo

--- src/Components/Common/Header.js
+++ src/Components/Common/Header.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import Logo from "./coin-logo.png";
 import "./Header.css";
 
 const Header = () => {
   return (
     <div className="Header">
-      <img src={Logo} alt="CoinCheck" className="Main-logo" />
-      <h1>CoinCheck</h1>
+      <Link to="/">
+        <img src={Logo} alt="CoinCheck" className="Main-logo" />
+        <h1>CoinCheck</h1>
+      </Link>
     </div>
   );
 };
--- src/Components/MissingPage/MissingPage.js
+++ src/Components/MissingPage/MissingPage.js
@@ -6,7 +6,7 @@ import "./MissingPage.css";
 const MissingPage = () => {
   return (
     <div className="missing-page">
-      <h1>Page not found</h1>
+      <h1>Yikes! Page not found.</h1>
 
       <Link to="/" className="missing-page-link"> Go Back </Link>
     </div>
