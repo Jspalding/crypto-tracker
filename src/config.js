d452a59e0735e98572ab8bae06e08d10bd27cd22
Jspalding <jamescodebase@outlook.com>
2018-05-31 17:13:51 +0100

    Minor fix to API URL

--- src/config.js
+++ src/config.js
@@ -2,4 +2,4 @@
 //example params ?limit=100 (returns top 100 cryptos)
 // ?convert=GBP&limit=100 (returns top 100 cryptos in GBP)
 export const API_URL = "https://api.coinmarketcap.com/v1/ticker/";
-export const COINCAP_URL ="http://coincap.io";
+export const COINCAP_URL ="https://coincap.io";
